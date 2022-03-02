1376953bea5233f73efb2a973777428d0bd4542d
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-01 23:21:12 -0300

    Add files via upload

---
+++ aula6-4.py
@@ -0,0 +1,11 @@
+lista = ["ornitorrinco","cachorro", "gato", "gato", "cachorro", "elefante", "ornitorrinco","ornitorrinco"]
+print(lista)
+print()
+
+conjunto_animais = set(lista) #set não tem índices repetidos
+print(conjunto_animais)
+print()
+
+lista_animais = list(conjunto_animais)
+print(lista_animais)
+print()
