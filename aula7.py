0775b8cd5fff7091dd4f5fd76a0a180f68e0011e
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-01 23:30:53 -0300

    Add files via upload

---
+++ aula7.py
@@ -0,0 +1,22 @@
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    return a / b
+print(soma(1,2))
+print(soma(3,4))
+print()
+print(subtracao(3,1))
+print(subtracao(14,3))
+print()
+print(multiplicacao(3,3))
+print(multiplicacao(4,6))
+print()
+print(divisao(25,5))
+print(divisao(70,7))
