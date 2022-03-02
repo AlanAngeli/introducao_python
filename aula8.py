bd622ea8739755da02a51ec9daa1633fc8fd211c
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-02 00:37:53 -0300

    Update aula8.py

--- aula8.py
+++ aula8.py
@@ -6,6 +6,7 @@ Podem interagir entre eles
 
 from aula7_3 import Televisao
 from aula7_calculadora import Calculadora
+from aula8_2 import contador_letras
 
 if __name__=='__main__':
     televisao = Televisao()
@@ -17,4 +18,10 @@ if __name__=='__main__':
 
     calculadora = Calculadora(5,10)
     print(calculadora.soma())
+
+    print()
+
+    lista = ["cachorro", "gato", "elefante"]
+    total_letras = contador_letras(lista)
+    print("Total de letras por lavra da listas: {}".format(total_letras))
 
