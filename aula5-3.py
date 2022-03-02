5f46481ce2b6e35816f9766aa5a16b71fdaed31e
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-01 22:41:14 -0300

    Add files via upload

---
+++ aula5-3.py
@@ -0,0 +1,12 @@
+lista = [12,10,7,5]
+lista_animais = ["Cachorro", "Gato", "Elefante", "Lobo", "Arara"]
+
+print(lista)
+print(lista_animais)
+
+lista.sort() #ordena a lista
+lista_animais.sort() #ordena a lista animal
+print()
+
+print(lista)
+print(lista_animais)
