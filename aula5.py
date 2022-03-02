fdcbf3a15845fe7499632c8d0cc9659c69ad8966
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-01 22:33:49 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,18 @@
+lista = [1,3,5,7]
+lista_animais = ["Cachorro", "Gato", "Elefante"]
+print(lista_animais[0]) #cachorro
+print(lista_animais[1]) #gato
+print(lista_animais[2]) #elefante
+print()
+soma = 0
+for x in lista:
+    print(x)
+    soma += x
+print(soma)
+print()
+#ou
+print(sum(lista))
+print()
+print(min(lista)) #valor mimino da lista
+print(max(lista)) #valor máximo da lista)
+
