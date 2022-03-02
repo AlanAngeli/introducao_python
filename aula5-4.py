b88ae552942f8113f04ef5a4f1a648409b3525af
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-01 22:43:17 -0300

    Add files via upload

---
+++ aula5-4.py
@@ -0,0 +1,6 @@
+lista = [12,10,7,5]
+lista_animais = ["Cachorro", "Gato", "Elefante", "Lobo", "Arara"]
+
+lista_animais[0] = "Macaco" #Adiciona no índice escolhido da lista
+lista_animais.append("Ovelha") #Adciona no final da lista
+print(lista_animais)
