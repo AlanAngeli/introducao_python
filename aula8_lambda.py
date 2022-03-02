04e5a18a05e1fddd3f15d056dd339877657301ed
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-02 00:50:29 -0300

    Add files via upload

---
+++ aula8_lambda.py
@@ -0,0 +1,10 @@
+contador_letras = lambda lista: [len(x) for x in lista]
+
+lista_animais = ["cachorro", "gato", "ornitorrinco"]
+contador_letras(lista_animais)
+
+soma = lambda a, b: a + b
+subtracao = lambda a, b: a - b
+
+print(soma(5,10))
+print(soma(12,8))
