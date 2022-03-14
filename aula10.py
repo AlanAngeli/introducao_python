81d0d9debc9d1e00e55bd02414231090ce9686d3
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-14 01:52:56 -0300

    Update aula10.py

--- aula10.py
+++ aula10.py
@@ -10,7 +10,40 @@
 " - Alterar formatação do DATETIME" \
 " - Realizar soma e subtração de datas com TIMEDELTA"
 
-from datetime import date, time
+from datetime import date, time, datetime, timedelta
+
+def trabalhando_com_datetime():
+    data_atual = datetime.now()
+    print(data_atual)
+    print(data_atual.strftime('Data: %d/%m/%Y Hora: %H:%M:%S'))
+    print(data_atual.strftime('%c'))
+    print(data_atual.year)
+    print(data_atual.day)
+    print(data_atual.month)
+    print()
+    #traduzir para o português:
+    print("TRADUZIR PARA PORTUGUÊS")
+    tupla = ('Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo')
+    print(tupla[data_atual.weekday()])
+    print()
+
+    #Converter data:
+    print("CONVERTER DATA")
+    data_criada = datetime(2018, 6, 20, 15, 30, 20) #Ano, mês, dia, hora, minutos, segundos
+    print(data_criada)
+    print(data_criada.strftime('%c'))
+    #data_string = '01/01/2019'
+    #data_convertida = datetime.strptime(data_string, '%d/%m/%Y')#só a data
+    data_string = '01/01/2019 12:20:22'
+    data_convertida = datetime.strptime(data_string, '%d/%m/%Y  %H:%M:%S')  #data e hora
+    print(data_convertida)
+    print()
+
+    ##Subtração:
+    print("SUBTRAINDO DATAS")
+    nova_data = data_convertida - timedelta(days=365, hours=2) #tirou 365 dias e duas horas
+    print(nova_data)
+
 
 def trabalhando_com_date():
     data_atual = date.today()
@@ -25,4 +58,12 @@ def trabalhando_com_date():
 
 def trabalhando_com_time():
     horario = time(hour=15, minute=18, second=30)
-    print(horario.strftime('%H:%M:%S'))
+    print(horario)
+    horario_str = horario.strftime('%H:%M:%S')
+    print(horario_str)
+    print(type(horario_str))
+
+if __name__ == '__main__':
+    #trabalhando_com_date()
+    #trabalhando_com_time()
+    trabalhando_com_datetime()
