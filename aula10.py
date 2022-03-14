298cec46664b2114c0d4513521decbb8ee6fe31a
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-14 01:16:42 -0300

    Update aula10.py

--- aula10.py
+++ aula10.py
@@ -10,3 +10,19 @@
 " - Alterar formatação do DATETIME" \
 " - Realizar soma e subtração de datas com TIMEDELTA"
 
+from datetime import date, time
+
+def trabalhando_com_date():
+    data_atual = date.today()
+    print(data_atual) #anom mês e dia com hífen
+    print(data_atual.strftime('%d/%m/%y')) #dia, mês e ano com barra
+    print(data_atual.strftime('%d %m %y')) #dia, mês e ano com espaço
+    print(data_atual.strftime('%A %B %Y'))
+    print()
+    print(type(data_atual))
+    data_atual_string = data_atual.strftime(('%d/%m/%y')) #convertendo para str
+    print(type(data_atual_string))
+
+def trabalhando_com_time():
+    horario = time(hour=15, minute=18, second=30)
+    print(horario.strftime('%H:%M:%S'))
