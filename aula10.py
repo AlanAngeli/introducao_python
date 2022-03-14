dadc8450ebc9d4ff9f5a6f64b3e80901fbec8165
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-14 00:42:55 -0300

    Update aula10.py

--- aula10.py
+++ aula10.py
@@ -1,2 +1,12 @@
-#####
+"1 - Manipular data e hora"
+"2 - Realizar conversão de texto para data " \
+"3 - Realizar soma e subtração em datas"
+
+"Detalhes:" \
+" - Como recuperar a data atual (DATE)" \
+" - Como trabalhar com a data, alterando sua formatação" \
+" - Como gerar m horário (TIME)" \
+" - Retornar data e hora atual (DATETIME)" \
+" - Alterar formatação do DATETIME" \
+" - Realizar soma e subtração de datas com TIMEDELTA"
 
