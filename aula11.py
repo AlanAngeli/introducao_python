e1af6cec9d1c48c890d4fac9fb772d20b9153d68
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-16 10:57:14 -0300

    Update aula11.py

--- aula11.py
+++ aula11.py
@@ -1,4 +1,11 @@
+lista = [1,10]
 try:
-    divisao = 10/0
+    divisao = 10/1
+    numero = lista[1]
+    x = a
 except ZeroDivisionError:
     print("Não é possível realizar uma divisão por zero")
+except IndexError:
+    print("Erro ao acessar um índice inválido da lista")
+except BaseException as ex:
+    print('Erro desconhecido. Erro: {}'.format(ex))
