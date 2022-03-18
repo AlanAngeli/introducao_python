46c920e16998c785c0c78b318b4864fa0c0ea76b
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-17 21:46:07 -0300

    Add files via upload

---
+++ aula12.py
@@ -0,0 +1,7 @@
+" que são pacotes"
+"O que é inalador de pacotes Python (PIP)" \
+"Como instalar um pacote Python" \
+"Como listar pacotes instalados no Python" \
+"Como utilizar um pacote" \
+"Instalar nosso primeiro pacote (Requests)" \
+"Realizar uma aquizição http com requests"
